d9897e9da0005218c8fdf5f0b63c4397dd18647f
lsaddan <lior.saddan.93@gmail.com>
2023-07-12 17:03:20 +0300

    Move prompts to json files outside of OpenAiClient

--- web_scraping/OpenAIClient.py
+++ web_scraping/OpenAIClient.py
@@ -5,19 +5,15 @@ import configparser
 
 
 class OpenAIClient:
-    def __init__(self, api_key, org_id):
-        print(f"api_key: {api_key[:10]}, org_id: {org_id}")
-        openai.organization = org_id
-        openai.api_key = api_key
-        role_mesage = [{'role': 'system', 'content' : 'You are an expert in secret management for machine to machine communication in enterprises. I will ask you give you descriptions such as "Easily manage your environment variables and secrets" and will ask you to output "true" or "false" if they are very related to machine to machine communication. Do not output anything else apart from "true" or "false"' },]
-        example_message = [{'role' : 'user', 'content' : 'Easily manage your environment variables and secrets'},
-                           {'role' : 'assistant', 'content' : 'True'},
-                           {'role' : 'user', 'content' : 'Making Internet Business Personal'},
-                           {'role' : 'assistant', 'content' : 'False'},
-                           {'role' : 'user', 'content' : 'A tool for aggregating multiple vaults'},
-                           {'role' : 'assistant', 'content' : 'True'},
-        ]
-        self.messages = role_mesage + example_message
+
+    def __init__(self, prompt):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        openai.api_key = open(config['general']['path_to_openai_api_key']).read().strip()
+        openai.organization = org_id = config['general']['org_id']
+        self.messages = prompt
+        print(__class__, self.messages)
+
 
 
     def is_description_related(self, description):
@@ -25,7 +21,7 @@ class OpenAIClient:
             return False
         messages = self.messages + [{'role' : 'user', 'content' : description},]
         completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, temperature=0)
-        return completion.choices[0].message.content.lower() == 'true'
+        return completion.choices[0].message.content
 
     
 
