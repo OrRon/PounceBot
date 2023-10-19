f4fe53ca2b3ca89091335f41216d74b81435f7b1
Orki <or.ron10@gmail.com>
2023-10-20 00:18:15 +0300

    Added helper to print json from pythong array

---
+++ linkedin_automation/dump_json.py
@@ -0,0 +1,34 @@
+import json  
+dict = {
+    'li_sugr': 'e43615a4-cef2-4592-87fa-b13b53141102',
+    'bcookie': '"v=2&891eab82-d9c8-4b1e-8b9c-bd44b4b43542"',
+    'bscookie': '"v=1&20230318153745c646639f-7c73-4437-8613-783c49179701AQE5iP5TT6Lv0zdWP9FWQUPC1ILYiP5H"',
+    'G_ENABLED_IDPS': 'google',
+    'aam_uuid': '92165267536254259991015519516893746916',
+    '_gcl_au': '1.1.374187996.1680543302',
+    'timezone': 'Asia/Jerusalem',
+    'li_theme': 'light',
+    'li_theme_set': 'app',
+    'li_rm': 'AQElCgM8m4KrNQAAAYdIMWZP_0LliL8WJ4kNc7gkgVKjeExH3YQwYRFVrUQ_tenXF8e7fgB3C_8u0E4BBtBpK1EBeloPNG_gdd5B2ZmEeabLLOgvVb_Utalh',
+    'visit': 'v=1&M',
+    'JSESSIONID': '"ajax:6136215520291738080"',
+    's_ips': '833',
+    'li_theme_set': 'app',
+    'gpv_pn': 'mobile.linkedin.com%2F',
+    's_tslv': '1690123123715',
+    'mbox': 'session#6f08301eed594a4db2c9d1e347675d13#1690124984|PC#6f08301eed594a4db2c9d1e347675d13.37_0#1705675124',
+    's_tp': '1096',
+    'liap': 'true',
+    'lang': 'v=2&lang=en-us',
+    'AMCVS_14215E3D5995C57C0A495C55%40AdobeOrg': '1',
+    'li_at': 'AQEDAQMNzNICtY5TAAABirJ3FAQAAAGLXBN_Zk0AAaWB-S73SL3LTuk71O2LGpUBU8rY5dMQGln79aeTEGgbvmQoamZlM4cp4xSoqQ8MWaAGP13o2_zGoTOvZxRKoDowPjHFlTLNzPgOmeU0WBBGqPjQ',
+    'AnalyticsSyncHistory': 'AQIqvovH9kgnwAAAAYs_BN9vG19U4fMgcC31IiRj2UtoXHWxA_3Dyw-DiEW8f125rzwlmBIQmEpCfs87EAxs2w',
+    'lms_ads': 'AQF56FokJRnt9wAAAYs_BODxmbel5xVTy2Z6uATi4Vt3_RrHGJKy4XxFJj3y2gnnsfnzqjur743ZCBvN9--f8hgS2XwEhe01',
+    'lms_analytics': 'AQF56FokJRnt9wAAAYs_BODxmbel5xVTy2Z6uATi4Vt3_RrHGJKy4XxFJj3y2gnnsfnzqjur743ZCBvN9--f8hgS2XwEhe01',
+    'AMCV_14215E3D5995C57C0A495C55%40AdobeOrg': '-637568504%7CMCIDTS%7C19650%7CMCMID%7C91627158154925003561069876089261614383%7CMCAAMLH-1698348958%7C6%7CMCAAMB-1698348958%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1697751358s%7CNONE%7CvVersion%7C5.1.1%7CMCCIDH%7C1330090450',
+    'UserMatchHistory': 'AQKD_xvqp2yeTgAAAYtJcRpn-xTOCVN7WEm4dfJ6WMFFJvcKEdEiCX5xgHW6uvX1MUn2sRZmNyDptkwX08eTmsQgHAeE2Vo9PimQsfvcF-VCCLMRWOEYeU2WY8UcnkV-cVPmrY77iO2_4neODFLHgnGIoOAhmvGJj_PySkbExO-vqJ6Ky2m2pGNORNJfOt4mayh_7AGyZJEaQ2HO4z6ToqPdoV3ZOa9HzUCu7yyPq_3ts76ReDdJqPivyGKzwlXF-TdziTbSIZ6bIpUySs-NFFf9WI9YtH_qFo2pl_p9ScRblyVdhPLWASd-SEx6dNSSvbCFdvg6RZtBuKjpJBfZegJ4qe5zk0I',
+    'lidc': '"b=TB50:s=T:r=T:a=T:p=T:g=4693:u=470:x=1:i=1697744243:t=1697745008:v=2:sig=AQEXmR00185KhEOeEV6tMFIS2hh9IBpf"',
+}
+
+json_object = json.dumps(dict, indent = 4)  
+print(json_object) 
